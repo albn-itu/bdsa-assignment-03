95f0463a28b83490db04dc2d2c4de927584042b4
albe2669 <albert@risenielsen.dk>
2021-09-28 12:54:20 +0200

    Copied original extensions

--- Assignment3.Tests/ExtensionsTests.cs
+++ Assignment3.Tests/ExtensionsTests.cs
@@ -6,9 +6,32 @@ namespace BDSA2020.Assignment02.Tests
     public class ExtensionsTests
     {
         [Fact]
-        public void Test1()
-        {
+        public void Flatten_given_lists_return_content() {
+            // Arrange
+            var list = new[]{
+                new[] {1,2,3,4},
+                new[] {5,6,7,8},
+                new[] {9,10}
+            };
 
+            // Act
+            var output = Iterators.Flatten<int>(list);
+
+            // Assert
+            Assert.Equal(new[] {1,2,3,4,5,6,7,8,9,10}, output);
+        }
+
+        [Fact]
+        public void Filter_given_1_2_3_4_5_return_2_and_4() {
+            // Arrange
+            var list = new[]{1,2,3,4,5};
+            Predicate<int> predicate = (int i) => i % 2 == 0;
+
+            // Act
+            var output = Iterators.Filter<int>(list, predicate);
+
+            // Asset
+            Assert.Equal(new[] {2,4}, output);
         }
     }
 }
--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -4,5 +4,22 @@ namespace BDSA2020.Assignment02
 {
     public static class Extensions
     {
+        public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items)
+        {
+            foreach(var list in items) {
+                foreach(var item in list) {
+                    yield return item;
+                }
+            }
+        }
+
+        public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate)
+        {
+            foreach(var item in items) {
+                if (predicate.Invoke(item)) {
+                    yield return item;
+                }
+            }
+        }
     }
 }
