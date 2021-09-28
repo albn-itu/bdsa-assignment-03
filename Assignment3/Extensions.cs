93988650dda5c009d8a8fc4bb444a28def9913e8
devilpant <peterbruhn_nielsen@yahoo.com>
2021-09-28 15:25:57 +0200

    Extensions and added tests

--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -1,25 +1,22 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;  
+using System.Text.RegularExpressions;
 
-namespace BDSA2020.Assignment02
+namespace Assignment3
 {
     public static class Extensions
     {
-        public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items)
-        {
-            foreach(var list in items) {
-                foreach(var item in list) {
-                    yield return item;
-                }
-            }
-        }
+        public static IEnumerable<int> Flatten(this IEnumerable<int>[] xs) => xs.SelectMany(i => i);
 
-        public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate)
-        {
-            foreach(var item in items) {
-                if (predicate.Invoke(item)) {
-                    yield return item;
-                }
-            }
-        }
+        public static IEnumerable<int> Filter(this int[] ys) => ys.Where(i => i > 42).Where(i => i % 7 == 0);
+        
+        public static IEnumerable<int> IsLeapYear(this int[] ys) => ys.Where(i => DateTime.IsLeapYear(i));
+
+        public static bool IsSecure(Uri uri) => (uri.Scheme.Contains("http")) && (uri.Scheme.Contains("https"));
+
+        public static int WordCount(this string s) => Regex.Split(s, @"[\P{L}]+").Length;
     }
+
+
 }
